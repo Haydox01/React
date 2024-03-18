bbc81ed38988f0d2253420c8f845fbb62f0da9d2
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-03-18 21:56:45 +0100

    m

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 import { Component } from 'react';
 import Greet from './Components/Greet';
 import Welcome from './Components/Welcome';
+import Hello from './Components/Hello';
 
 class App extends Component {
   render(){
    return(
     <div className="App">
-      <Greet />
-      <Welcome />
+      <Greet name = "Bruce" heroName= "Batman" />
+      <Greet name = "Clark" heroName= "Spiderman" />
+      <Greet name = "James" />
+
+
+
+      {/* <Welcome />
+      <Hello /> */}
     </div>
     );
   }
--- hello-world/src/Components/Greet.js
+++ hello-world/src/Components/Greet.js
@@ -3,5 +3,8 @@ import React from 'react';
 // function Greet(){
 //     return <h1>Hello vishwas</h1>
 // }
-const Greet=()=><h1>Hello vishwas</h1>
+const Greet= props => {
+    console.log(props)
+    return <h1>Hello {props.name} a.k.a {props.heroName}</h1>
+}
 export default Greet;
