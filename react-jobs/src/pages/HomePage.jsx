6fdcc09127c5f330e319aeb73038a03edf293bf2
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-06-25 19:23:52 +0100

    fetch

--- react-jobs/src/pages/HomePage.jsx
+++ react-jobs/src/pages/HomePage.jsx
@@ -3,7 +3,6 @@ import Hero from "../components/Hero";
 import HomeCards from "../components/HomeCards";
 import JobListings from "../components/JobListings";
 import ViewAllJobs from "../components/ViewAllJobs";
-import NavBar from "../components/NavBar";
 
 function HomePage() {
   return (
